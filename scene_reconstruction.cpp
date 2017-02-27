58fa283f22e69bc16d655dc43feb14da8e819c7e
Guojun Yang <gyang44@outlook.com>
2017-02-27 17:27:03 -0600

    implemented cuda feature detector, feature matcher to be implemented

--- scene_reconstruction.cpp
+++ scene_reconstruction.cpp
@@ -8,9 +8,10 @@
 #include <opencv2/viz.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <opencv2/core/cuda.hpp>
-
+#include <opencv2/cudaimgproc.hpp>
 #include <opencv2/cudafeatures2d.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/xfeatures2d.hpp>
@@ -44,34 +45,31 @@ int main(int argc, char* argv[])
         cv::cuda::printCudaDeviceInfo(0);
     }
 
-    for(int feature = cv::cuda::FEATURE_SET_COMPUTE_10; feature != cv::cuda::DYNAMIC_PARALLELISM; feature++)
-    {
-        if (cv::cuda::deviceSupports( (cv::cuda::FeatureSet)feature))
-        {
-            std::cout << "yes" << std::endl;
-        }
-        else
-        {
-            std::cout << "no" << std::endl;
-        }
-    }
+    cv::namedWindow("cudaFeature", cv::WINDOW_OPENGL);
+    //cv::cuda::setGlDevice();
 
     cv::cuda::setDevice(0);
 
     environment workEnv("../setting.xml");
 
-    cv::Ptr<cv::FastFeatureDetector> CudaDetector = cv::cuda::FastFeatureDetector::create();
-
+    cv::Ptr<cv::cuda::ORB> CudaDetector = cv::cuda::ORB::create(500, 1.2f, 8, 31, 0, 2, 0, 31, 20, true);
+    cv::cuda::GpuMat newFrameGpu, newFrameGpuGray;
     cv::Mat newFrame;
     cv::cuda::GpuMat newFrameFeature;
-    cv::cuda::GpuMat frameTest(100,100, NULL);
+    std::vector<std::vector<cv::KeyPoint>> KeyPoint;
+    std::vector<cv::Mat> descriptor;
     while(workEnv.video.grab())
     {
         workEnv.video.retrieve(newFrame);
-        std::vector<cv::KeyPoint> Keypoints;
-        CudaDetector -> detect(frameTest, Keypoints);
-        //drawKeypoints( newFrame, Keypoints, newFrameFeature, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT );
-        //cv::imshow("cudaFeature", newFrameFeature);
+        newFrameGpu.upload(newFrame);
+        cv::cuda::cvtColor(newFrameGpu, newFrameGpuGray, CV_RGB2GRAY);
+        std::vector<cv::KeyPoint> frameKeypoints;
+        cv::cuda::GpuMat frameDescriptor;
+
+        CudaDetector -> detectAndCompute(newFrameGpuGray, cv::cuda::GpuMat(), frameKeypoints, frameDescriptor);
+        cv::drawKeypoints( newFrame, frameKeypoints, newFrame, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT );
+        newFrameGpu.upload(newFrame);
+        cv::imshow("cudaFeature", newFrameGpu);
         cv::waitKey(10);
     }
 
