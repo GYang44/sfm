e8ea30dd06ecf2ccae44db4766d719861b6f6421
Guojun Yang <gyang44@outlook.com>
2017-04-10 00:31:39 -0500

    finish implementing the reconstruction use CPU and BA, rendering bug needs to be fixed and reslut is not visualized yet

--- scene_reconstruction.cpp
+++ scene_reconstruction.cpp
@@ -4,18 +4,13 @@
 #include <fstream>
 #include <string>
 #include <thread>
+#include <stdlib.h>
 
 #include <opencv2/sfm.hpp>
 #include <opencv2/viz.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
-#include <opencv2/core/cuda.hpp>
-#include <opencv2/cudaimgproc.hpp>
-#include <opencv2/cudafeatures2d.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/xfeatures2d.hpp>
 
 #include <opencv2/sfm.hpp>
 
@@ -40,109 +35,47 @@ void camPoseFromVideo()
     // Read input parameters
     //sfmEnviroment wkEnv(std::string("../setting.xml"));
     environment workEnv("../setting.xml");
-    //create new window set device
-    cv::namedWindow("cudaFeature", cv::WINDOW_OPENGL);
-    cv::cuda::setDevice(0);
-
-    //create feature detector
-    cv::cuda::SURF_CUDA CudaDetector(10);
-
-    //tmp variable for frame handling
-    cv::cuda::GpuMat newImgFrameGpu, newImgFrameGpuGray;
-    cv::Mat newImgFrame;
+    system("exec rm ./tmp/*");
 
     //frames of keypoints and descriptors for entire video
-    std::vector<frame> frames;
-    std::vector<cv::Mat>  segmentFrames;
+    std::vector<std::string> images;
 
-    cv::Ptr<cv::cuda::DescriptorMatcher> matcher = cv::cuda::DescriptorMatcher::createBFMatcher(cv::NORM_L2);
+    cv::Mat newImgFrame;
 
-    cv::Mat oldFrame, drawFrame;
+    uint frameCounter(0);
+
+    cv::Matx33d K = (workEnv.cameraMatrix.at<double>(0,0), 0, workEnv.cameraMatrix.at<double>(0,2), 0, workEnv.cameraMatrix.at<double>(1,1), workEnv.cameraMatrix.at<double>(1,2), 0, 0, 1);
 
     while(workEnv.video.grab())
     {
         //convert new frame to gray scale on GPU
         workEnv.video.retrieve(newImgFrame);
-        newImgFrameGpu.upload(newImgFrame);
-        cv::cuda::cvtColor(newImgFrameGpu, newImgFrameGpuGray, CV_RGB2GRAY);
+        frameCounter++;
 
-        //detect keypoints and compute descriptors
-        std::vector<cv::KeyPoint> frameKeypoints;
-        cv::cuda::GpuMat frameDescriptorsGpu;
-        CudaDetector(newImgFrameGpuGray, cv::cuda::GpuMat(), frameKeypoints, frameDescriptorsGpu);
-
-        //create new regular frame with detected keypoints
-        frames.push_back(frame(frameKeypoints));
-
-        //matches in from the new frame
-        cv::cuda::GpuMat newMatchsGpu;
-
-        std::vector<std::vector<cv::DMatch>> newMatchs;
-
-        // check if its the first frame;
-        if (frames.size() > 1)
+        if ((frameCounter % 5) == 0)
         {
-            //specify key frame for current frame
-            frames[frames.size() - 1].keyframe = frames[frames.size() - 2].keyframe;
-
-            //find matches, in newMatchsGpu or newMatchs, the first element contains the candidates in second descriptors matching to first element in first descriptor
-            matcher -> knnMatchAsync(frameDescriptorsGpu, frames[frames[frames.size() - 2].keyframe].descriptors, newMatchsGpu, 1);
-            matcher -> knnMatchConvert(newMatchsGpu, newMatchs, false);
-
-            //matched keypoint after remove outliner
-            int matchedKeypoint = matchForCamPose(frames[frames[frames.size() - 2].keyframe], frames[frames.size() - 1], newMatchs);
-
-            // if not add new key frame
-            if (matchedKeypoint <= 10 | segmentFrames.size() >= 5)
-            {
-
-                if (matchedKeypoint < 7)
-                {
-                    std::cout << "failed to find enough points, last frame erased" << std::endl;
-                    frames.pop_back();
-                }
-                else
-                {
-                    std::cout << "start a new local reconstruction" << std::endl;
-                    // add new keyframe and reconstruct
-                    /*
-                    std::vector<cv::Mat> Rs, Ts, point3d;
-                    cv::sfm::reconstruct(segmentFrames, Rs, Ts, workEnv.cameraMatrix, point3d, true);
-                    for (int i(0); i < Rs.size(); i++)
-                    {
-                        camera.updateRT(Rs[i], Ts[i]);
-                        camera.updateWrP(camera);
-                    }
-                    */
-                    // reset segmentFrames after reconstruction
-                    segmentFrames.clear();
-                    segmentFrames.push_back(frames[frames.size() - 1].keypointAsKeyframe);
-
-                }
-            }
-            else
-            {
-                // add keypoints of a regular frame to the segment
-                segmentFrames.push_back(frames[frames.size() - 1].keypointToKeyframe);
-                std::cout << "just another frame" << std::endl;
-            }
-
-            cv::drawMatches(newImgFrame, frames[frames.size() - 1].keypoint, oldFrame, frames[frames.size() - 2].keypoint, newMatchs, drawFrame);
-            newImgFrameGpu.upload(drawFrame);
-            cv::imshow("cudaFeature", newImgFrameGpu);
-
+            //store image
+            images.push_back("./tmp/" + std::to_string(frameCounter) + ".png");
+            cv::imwrite(images.back(), newImgFrame);
         }
-        // create keyframe if it's the first frame
-        else
+        if ((frameCounter % 25) == 0)
         {
-            //create first frame as key frame
-            frameDescriptorsGpu.copyTo(frames[0].descriptors);
-            frames[0].keyframe = 0;
-            oldFrame = newImgFrame;
+            //reconstruct
+            cv::Mat points3d;
+            std::vector<cv::Mat> Rs, Ts;
+            cv::sfm::reconstruct(images, Rs, Ts, K, points3d, true);
+            for(uint i(0); i < Rs.size(); i++)
+            {
+                camera.updateRT(Rs[i], Ts[i]);
+                camera.updateWrP(camera);
+            }
+            images.clear();
+            std::cout << "finished reconstruction for frame " << std::to_string(frameCounter) << std::endl;
+            std::cout << "press any key to countinue" << std::endl;
+            cv::waitKey();
         }
-
-        if (cv::waitKey(30) == 27) break;
-
+        cv::imshow("newFrame", newImgFrame);
+        if (cv::waitKey(10) == 27) break;
     }
 
 }
